102c14253a2676d5b5dea27e0ead71707812407e
Gojek <gojek@Gojeks-MacBook-Pro.local>
2017-05-30 12:32:50 +0530

    division of classes

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,103 +1,7 @@
-//Point class that represents a point on a grid
-var Point = function (x,y,alive){
-  this.x = x;
-  this.y = y;
-  this.alive = alive;
-}
-
-Point.prototype = {
-  isAlive : function(){
-    return this.alive;
-  }
-}
-
-function getPoint(x,y){
-  return "x" + x + "y" +y;
-}
-//Grid class that represents the board
-var grid = function(){
-  this.points = {};
-}
-
-grid.prototype = {
-  addPoint : function(point){
-    this.points[getPoint(point.x,point.y)] = point;
-
-  },
-  getPointAt : function(x,y){
-    return this.points[getPoint(x,y)];
-  },
-  aliveNeighbours : function(point){
-    var x = point.x;
-    var y = point.y;
-    var alivePoints = 0;
-    var r = [-1,-1,-1,0,0,1,1,1];
-    var c = [-1,0,1,-1,1,-1,0,1];
-    for(var i = 0; i<8; i++)
-    {
-      var temp_x = x + r[i];
-      var temp_y = y + c[i];
-      if(temp_y >=0 && temp_y < col && temp_x < row && temp_x >=0)
-       if(this.getPointAt(temp_x,temp_y).alive)
-        alivePoints++;
-    }
-    return alivePoints;
-  },
-  fill_grid : function(r,c){
-    for(var i = 0 ; i<r ; i++){
-      for(var j = 0 ; j<c ; j++){
-        var random = Math.random();
-        var point;
-        if(random > 0.7)
-        {
-          point = new Point(i,j,true);
-        }
-        else
-        {
-          point = new Point(i,j,false);
-        }
-        this.addPoint(point);
-      }
-    }
-  },
-  next_state : function(point){
-    var x = point.x;
-    var y = point.y;
-    var count = this.aliveNeighbours(point);
-    var state = 0;
-    if(point.alive){
-      switch(count){
-        case 0:
-        case 1:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        state = 0;
-        break;
-        case 2:
-        case 3:
-        state = 1;
-        break;
-      }
-    }else{
-      switch(count){
-        case 3:
-        state = 1;
-        break;
-        default:
-        state = 0;
-      }
-    }
-    return state;
-  }
-
-}
-
 //#######################   MAIN   #################################
 var row = 400;
 var col = 400;
+
 
 var real_grid = new grid;
 var dummy_grid = new grid;
@@ -138,6 +42,7 @@ function update_grid(r,c){
   }
 }
 
+
 function show(){
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
@@ -151,12 +56,3 @@ function show(){
     }
   }
 }
-
-
-module.exports = {
-  Point: Point,
-  grid: grid,
-  getPoint: getPoint,
-  gol: gol
-};
-
--- test/gameOfLife_test.js
+++ test/gameOfLife_test.js
@@ -1,14 +1,15 @@
 var assert = require('assert');
-var gameOfLife = require('../src/gameOfLife');
+var classes = require('../src/classes');
 
 
-describe('gameOfLife', function(){
+
+describe('classes', function(){
 
 	describe('isAlive', function(){
 		it('returns 1 when the point is alive', function(){
 
-			var grid = new gameOfLife.grid;
-			var point = new gameOfLife.Point(2,2,true);
+			var grid = new classes.grid;
+			var point = new classes.Point(2,2,true);
 
 			grid.addPoint(point);
 			assert.equal(1, point.isAlive());
@@ -18,14 +19,14 @@ describe('gameOfLife', function(){
 	describe('getPoint', function(){
 		it('returns x0y0 when input is 0,0', function(){
 
-			assert.equal("x0y0", gameOfLife.getPoint(0,0));
+			assert.equal("x0y0", classes.getPoint(0,0));
 
 		})
 	})
 	describe('addPoint', function(){
 		it('adds a point to the grid', function(){
-			var grid = new gameOfLife.grid;
-			var point = new gameOfLife.Point(2,2,true);
+			var grid = new classes.grid;
+			var point = new classes.Point(2,2,true);
 
 			grid.addPoint(point);
 
@@ -35,8 +36,8 @@ describe('gameOfLife', function(){
 	})
 	describe('getPointAt', function(){
 		it('returns the point at given position', function(){
-			var grid = new gameOfLife.grid;
-			var point = new gameOfLife.Point(2,2,true);
+			var grid = new classes.grid;
+			var point = new classes.Point(2,2,true);
 
 			grid.addPoint(point);
 
@@ -46,16 +47,16 @@ describe('gameOfLife', function(){
 	})
 	describe('aliveNeighbours', function(){
 		it('returns the number of alive neighbours of a given point', function(){
-			var grid = new gameOfLife.grid;
-			var point1 = new gameOfLife.Point(0,0,true);
-			var point2 = new gameOfLife.Point(0,1,false);
-			var point3 = new gameOfLife.Point(0,2,false);
-			var point4 = new gameOfLife.Point(1,0,true);
-			var point5 = new gameOfLife.Point(1,1,true);
-			var point6 = new gameOfLife.Point(1,2,true);
-			var point7 = new gameOfLife.Point(2,0,true);
-			var point8 = new gameOfLife.Point(2,1,true);
-			var point9 = new gameOfLife.Point(2,2,true);
+			var grid = new classes.grid;
+			var point1 = new classes.Point(0,0,true);
+			var point2 = new classes.Point(0,1,false);
+			var point3 = new classes.Point(0,2,false);
+			var point4 = new classes.Point(1,0,true);
+			var point5 = new classes.Point(1,1,true);
+			var point6 = new classes.Point(1,2,true);
+			var point7 = new classes.Point(2,0,true);
+			var point8 = new classes.Point(2,1,true);
+			var point9 = new classes.Point(2,2,true);
 
 			grid.addPoint(point1);
 			grid.addPoint(point2);
@@ -73,16 +74,16 @@ describe('gameOfLife', function(){
 	})
 	describe('next_state', function(){
 		it('returns next state of a point', function(){
-			var grid = new gameOfLife.grid;
-			var point1 = new gameOfLife.Point(0,0,true);
-			var point2 = new gameOfLife.Point(0,1,false);
-			var point3 = new gameOfLife.Point(0,2,false);
-			var point4 = new gameOfLife.Point(1,0,true);
-			var point5 = new gameOfLife.Point(1,1,true);
-			var point6 = new gameOfLife.Point(1,2,true);
-			var point7 = new gameOfLife.Point(2,0,true);
-			var point8 = new gameOfLife.Point(2,1,true);
-			var point9 = new gameOfLife.Point(2,2,true);
+			var grid = new classes.grid;
+			var point1 = new classes.Point(0,0,true);
+			var point2 = new classes.Point(0,1,false);
+			var point3 = new classes.Point(0,2,false);
+			var point4 = new classes.Point(1,0,true);
+			var point5 = new classes.Point(1,1,true);
+			var point6 = new classes.Point(1,2,true);
+			var point7 = new classes.Point(2,0,true);
+			var point8 = new classes.Point(2,1,true);
+			var point9 = new classes.Point(2,2,true);
 
 			grid.addPoint(point1);
 			grid.addPoint(point2);
